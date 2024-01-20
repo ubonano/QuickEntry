6dbd1cffd0da52c706c77ae9ee7f5cd55ed97e5c
Uriel Bonano <ubonano@gmail.com>
2024-01-19 21:44:22 -0300

    add emualtors

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'config/firebase_options.dart';
@@ -7,6 +8,12 @@ import 'views/event_create_screen.dart';
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseFirestore.instance.settings = const Settings(
+    host: 'localhost:8080',
+    sslEnabled: false,
+    persistenceEnabled: true,
   );
 
   setupLocator();
