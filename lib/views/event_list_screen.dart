92c715ab6bfde531d0a419783b09390deaf5bce0
Uriel Bonano <ubonano@gmail.com>
2024-01-22 20:30:10 -0300

    refactor

--- lib/views/event_list_screen.dart
+++ lib/views/event_list_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-
 import '../config/get_it_setup.dart';
 import '../controllers/event_controller.dart';
 import '../models/event.dart';
 import '../widgets/event_list.dart';
+import '../widgets/generic_stream_builder.dart';
 import 'event_create_screen.dart';
 
 class EventsScreen extends StatefulWidget {
+  const EventsScreen({super.key});
+
   @override
   _EventsScreenState createState() => _EventsScreenState();
 }
@@ -15,36 +17,21 @@ class _EventsScreenState extends State<EventsScreen> {
   final EventController _eventController = getIt<EventController>();
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Eventos'),
       ),
-      body: StreamBuilder<List<Event>>(
+      body: GenericStreamBuilder<List<Event>>(
         stream: _eventController.eventStream,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else if (snapshot.hasData) {
-            return EventsList(events: snapshot.data!);
-          } else {
-            return const Text('No hay eventos disponibles.');
-          }
-        },
+        builder: (context, snapshot) => EventsList(events: snapshot.data!),
+        errorMessage: 'Error al cargar los eventos.',
+        emptyMessage: 'No hay eventos disponibles.',
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => const EventCreateScreen()),
-          );
-        },
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const EventCreateScreen()),
+        ),
         child: const Icon(Icons.add),
       ),
     );
