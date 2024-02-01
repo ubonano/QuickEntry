1578766aea8df0b9844a6e6f0547b37052b6427d
Uriel Bonano <ubonano@gmail.com>
2024-01-31 22:35:28 -0300

    Change routes

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,25 +1,20 @@
-import 'package:go_router/go_router.dart';
+import 'package:get/get.dart';
 
 import '../views/event_detail_screen.dart';
 import '../views/event_list_screen.dart';
 
-class AppRouter {
-  static GoRouter createRouter() {
-    return GoRouter(
-      routes: [
-        GoRoute(
-          path: '/',
-          builder: (context, state) => const EventsScreen(),
-        ),
-        GoRoute(
-          path: '/event/:eventId',
-          builder: (context, state) {
-            final eventId = state.pathParameters['eventId'] ?? '';
-            return EventDetailsScreen(eventId: eventId, showBackButton: false);
-          },
-        ),
-      ],
-      debugLogDiagnostics: true,
-    );
-  }
+class AppRoutes {
+  static final routes = [
+    GetPage(
+      name: '/',
+      page: () => const EventsScreen(),
+    ),
+    GetPage(
+      name: '/event/:eventId',
+      page: () {
+        var eventId = Get.parameters['eventId'] ?? '';
+        return EventDetailsScreen(eventId: eventId);
+      },
+    ),
+  ];
 }
